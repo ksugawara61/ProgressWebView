de90cb14903ebc1d14458a7c9c2d4d399fe7800e
ksugawara61 <katsuya@kokohadoko.net>
2020-08-11 22:05:34 +0900

    Initial Commit

---
+++ ProgressWebView/ProgressWebView.h
@@ -0,0 +1,19 @@
+//
+//  ProgressWebView.h
+//  ProgressWebView
+//
+//  Created by 菅原勝也 on 2020/08/11.
+//  Copyright © 2020 listadoko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProgressWebView.
+FOUNDATION_EXPORT double ProgressWebViewVersionNumber;
+
+//! Project version string for ProgressWebView.
+FOUNDATION_EXPORT const unsigned char ProgressWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressWebView/PublicHeader.h>
+
+
